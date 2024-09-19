d0dd417cbf3aabf98314c023c321e25be8c71310
mrkshovon <mrkshovon@gmail.com>
2024-09-19 11:36:12 -0400

    Update cdsenv.bash

--- cdsenv.bash
+++ cdsenv.bash
@@ -1,3 +1,4 @@
+#!/bin/bash
 echo 'license   VSELicenseCheckoutOrder   string   "XL"  '> ~/.cdsenv
 echo 'license   adeMaestroCheckoutOrder   string   "Maestro"  ' >> ~/.cdsenv
 echo 'license   maestroCheckoutOrder   string	 "Explorer,   Assembler" ' >> ~/.cdsenv
